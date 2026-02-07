1e9a4679abe468004d34a08032d8c3bfd2c53834
Nada Amin <namin@seas.harvard.edu>
2026-02-07 16:51:19 -0500

    tweaking-placement

--- src/App.tsx
+++ src/App.tsx
@@ -448,6 +448,12 @@ function App() {
                     </span>
                   )}
 
+                  {lap.expectedDuration >= 0 && (
+                    <span className="expected-duration">
+                      (was {formatTime(lap.expectedDuration)}, <span className={lap.duration >= lap.expectedDuration ? 'diff-plus' : 'diff-minus'}>{lap.duration >= lap.expectedDuration ? '+' : '-'}{formatTime(Math.abs(lap.duration - lap.expectedDuration))}</span>)
+                    </span>
+                  )}
+
                   {isEditingSection ? (
                     <input
                       ref={sectionInputRef}
@@ -508,12 +514,6 @@ function App() {
                       </button>
                     )}
                   </div>
-
-                  {lap.expectedDuration >= 0 && (
-                    <span className="expected-duration">
-                      (was {formatTime(lap.expectedDuration)}, <span className={lap.duration >= lap.expectedDuration ? 'diff-plus' : 'diff-minus'}>{lap.duration >= lap.expectedDuration ? '+' : '-'}{formatTime(Math.abs(lap.duration - lap.expectedDuration))}</span>)
-                    </span>
-                  )}
 
                   <button
                     className="move-btn"
